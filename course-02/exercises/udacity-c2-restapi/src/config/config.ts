f92d49ce0c6b56c7efdc7c0fa84cd58036f295f3
bvgohmslf <osumunemichael@gmail.com>
2021-09-22 13:35:56 +0100

    Implementing authentication and testing deployment to elastic beanstalk with postman

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
+    "username": "udagrambvgodev",
+    "password": "udagrambvgodev",
+    "database": "udagrambvgodev",
+    "host": "udagrambvgodev.caqhxnap7q3k.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-bvgo-bev"
   },
   "prod": {
     "username": "",
@@ -17,7 +17,7 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "secret": process.env.JWT_SECRET
+    "secret": "helloworld"
   }
 }
 
