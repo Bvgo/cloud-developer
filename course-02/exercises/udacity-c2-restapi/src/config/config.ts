1964586548495e5c54e5b91863f53c15aedcf7ca
bvgohmslf <osumunemichael@gmail.com>
2021-09-26 12:04:43 +0100

    resolve password issue

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,6 +1,6 @@
 export const config = { "dev": {
     "username": process.env.POSTGRESS_USERNAME,
-    "password" : process.env.POSTGRESS_PW,
+    "password" : process.env.POSTGRESS_PASSWORD,
     "database": process.env.POSTGRESS_DB,
     "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
